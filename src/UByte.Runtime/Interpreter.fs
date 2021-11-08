7df30bc409bfda5332032bccfba6db8a090969fc
David N <davidcalieln@gmail.com>
2021-11-07 19:20:03 -0800

    Add module signature for new interpreter module

---
+++ src/UByte.Runtime/Interpreter.fs
@@ -0,0 +1,53 @@
+﻿[<RequireQualifiedAccess>]
+module UByte.Runtime.Interpreter
+
+open System
+
+open UByte.Format.Model
+
+open UByte.Resolver
+open UByte.Runtime.MemoryManagement
+
+let inline isFlagSet flag value = value &&& flag = flag
+
+[<Sealed>]
+type MissingEntryPointException (message: string) = inherit Exception(message)
+
+[<Sealed>]
+type ModuleNotFoundException (name: ModuleIdentifier, message) =
+    inherit Exception(message)
+
+    member _.Name = name
+
+let moduleImportResolver (loader: ModuleIdentifier -> Module voption) =
+    let resolver = ref Unchecked.defaultof<_>
+    resolver.Value <- fun id ->
+        match loader id with
+        | ValueSome import -> ResolvedModule(import, resolver.Value)
+        | ValueNone -> raise(ModuleNotFoundException(id, "Unable to find module " + string id))
+    resolver.Value
+
+[<Sealed>]
+type Runtime
+    (
+        program: Module,
+        moduleImportLoader,
+        garbageCollectorStrategy: IGarbageCollector
+    )
+    =
+    let program = ResolvedModule(program, moduleImportResolver moduleImportLoader)
+
+    static member Initialize
+        (
+            program,
+            ?moduleImportLoader,
+            ?garbageCollectorStrategy
+        )
+        =
+        Runtime (
+            program,
+            defaultArg moduleImportLoader (fun _ -> ValueNone),
+            defaultArg garbageCollectorStrategy (CollectionStrategies.NaiveMarkAndSweep())
+        )
+
+    member _.Program = program
---
+++ src/UByte.Runtime/Interpreter.fsi
@@ -0,0 +1,29 @@
+﻿[<RequireQualifiedAccess>]
+module UByte.Runtime.Interpreter
+
+open UByte.Format
+open UByte.Resolver
+
+[<Sealed; Class>]
+type MissingEntryPointException = inherit System.Exception
+
+[<Sealed; Class>]
+type ModuleNotFoundException =
+    inherit System.Exception
+
+    member Name : Model.ModuleIdentifier
+
+[<Sealed>]
+type Runtime =
+    /// Initializes the interpreter with the specified program containing the code to run.
+    static member Initialize :
+        program: Model.Module *
+        ?moduleImportLoader: (Model.ModuleIdentifier -> Model.Module voption) *
+        ?garbageCollectorStrategy: UByte.Runtime.MemoryManagement.IGarbageCollector -> Runtime
+
+    member Program : UByte.Resolver.ResolvedModule
+
+    /// <summary>Invokes the entry point of the program, supplying the specified arguments.</summary>
+    /// <returns>The integer exit code returned by the program.</returns>
+    /// <exception cref="T:UByte.Runtime.Interpreter.MissingEntryPointException" />
+    member InvokeEntryPoint : argv: string[] -> int32
