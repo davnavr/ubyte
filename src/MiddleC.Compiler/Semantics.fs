6c45e9af9ffe3968efde963609b8d6bb01704b52
David N <davidcalieln@gmail.com>
2021-11-20 15:58:34 -0800

    Add wrapper types to differentiate between full names of namespaces and types

--- src/MiddleC.Compiler/Semantics.fs
+++ src/MiddleC.Compiler/Semantics.fs
@@ -9,13 +9,56 @@ open MiddleC.Compiler.Parser
 
 open UByte.Format
 
+[<Struct; NoComparison; StructuralEquality>]
+type FullNamespaceName =
+    internal
+    | FullNamespaceName of ParsedNamespaceName
+
+[<RequireQualifiedAccess>]
+module FullNamespaceName =
+    let empty = FullNamespaceName ImmutableArray.Empty
+
+    let append (nested: ParsedNamespaceName) (FullNamespaceName parent) =
+        FullNamespaceName(parent.AddRange nested)
+
+    let toParsedName (FullNamespaceName ns) = ns
+
+[<Struct; NoComparison; StructuralEquality>]
+type FullTypeIdentifier =
+    internal
+    | FullTypeIdentifier of TypeIdentifier
+
+    member this.Name = let (FullTypeIdentifier { TypeIdentifier.Name = name }) = this in name
+
+    member this.Namespace = let (FullTypeIdentifier { TypeIdentifier.Namespace = ns }) = this in FullNamespaceName ns
+
+    override this.ToString() = let (FullTypeIdentifier id) = this in id.ToString()
+
+[<RequireQualifiedAccess>]
+module FullTypeIdentifier =
+    let inline create tname tnamespace =
+        { TypeIdentifier.Name = tname
+          TypeIdentifier.Namespace = tnamespace }
+        |> FullTypeIdentifier
+
+    let ofTypeIdentifier (FullNamespaceName fnamespace) (id: inref<TypeIdentifier>) =
+        create id.Name (fnamespace.AddRange id.Namespace)
+
 type SemanticErrorMessage =
-    | DuplicateTypeDefinition of MiddleC.Compiler.Parser.TypeIdentifier
+    | DuplicateTypeDefinition of FullTypeIdentifier
+    | UndefinedTypeIdentifier of TypeIdentifier
+    | AmbiguousTypeIdentifier of TypeIdentifier * matches: seq<FullTypeIdentifier>
     | UnknownError of message: string
 
     override this.ToString() =
         match this with
         | DuplicateTypeDefinition id -> "Duplicate definition of type " + id.ToString()
+        | UndefinedTypeIdentifier id -> "The type definition " + id.ToString() + " does not exist"
+        | AmbiguousTypeIdentifier(id, matches) ->
+            System.Text.StringBuilder(id.ToString())
+                .Append(" could refer to any of the following types: ")
+                .AppendJoin(", ", matches)
+                .ToString()
         | UnknownError message -> message
 
 type SemanticError =
@@ -24,21 +67,25 @@ type SemanticError =
       Source: ParsedFile
       Message: SemanticErrorMessage }
 
+type CheckedTypeDefinition =
+    private
+        { identifier: FullTypeIdentifier
+          Source: ParsedFile
+          AttributeNodes: ParsedNodeArray<TypeAttributeNode>
+          InheritanceNodes: ParsedNodeArray<TypeIdentifier>
+          MemberNodes: ParsedNodeArray<TypeMemberNode>
+          UsedNamespaces: ImmutableArray<FullNamespaceName>
+          [<DefaultValue>] mutable flags: Model.TypeDefinitionFlags }
+
+    member this.Identifier = this.identifier
+
 [<RequireQualifiedAccess>]
 type CheckedModule =
-    { Errors: ImmutableArray<SemanticError> } 
+    { DefinedTypes: ImmutableArray<CheckedTypeDefinition>
+      Errors: ImmutableArray<SemanticError> }
 
 [<RequireQualifiedAccess>]
 module TypeChecker =
-    [<NoComparison; NoEquality>]
-    type private CheckingType =
-        { Identifier: TypeIdentifier
-          Source: ParsedFile
-          Attributes: ParsedNodeArray<TypeAttributeNode>
-          InheritedTypes: ParsedNodeArray<TypeIdentifier>
-          Members: ParsedNodeArray<TypeMemberNode>
-          UsedNamespaces: ImmutableArray<ParsedNamespaceName> }
-
     let private addErrorMessage (errors: ImmutableArray<_>.Builder) (node: ParsedNode<_>) source message =
         errors.Add { Line = node.Line; Column = node.Column; Source = source; Message = message }
 
@@ -47,11 +94,11 @@ module TypeChecker =
             errors
             file
             (lookup: Dictionary<_, _>)
-            currentNamespaceName
+            (currentNamespaceName: FullNamespaceName)
             (parentUsingDeclarations: ImmutableArray<_>)
             (nodes: ParsedNodeArray<TopLevelNode>)
             =
-            let usings = ImmutableArray.CreateBuilder()
+            let usings = ImmutableArray.CreateBuilder<FullNamespaceName>()
             let declaredTypeNodes = List()
             let nestedNamespaceDeclarations = List()
 
@@ -63,16 +110,14 @@ module TypeChecker =
                     nestedNamespaceDeclarations.Add(struct(ns, nested))
                 | TopLevelNode.TypeDeclaration(name, attributes, extends, members) ->
                     declaredTypeNodes.Add <| fun usedNamespaceDeclarations ->
-                        { CheckingType.Identifier =
-                            { TypeIdentifier.Name = name
-                              TypeIdentifier.Namespace = currentNamespaceName }
+                        { identifier = FullTypeIdentifier.create name (FullNamespaceName.toParsedName currentNamespaceName)
                           Source = file
-                          Attributes = attributes
-                          InheritedTypes = extends
-                          Members = members
+                          AttributeNodes = attributes
+                          InheritanceNodes = extends
+                          MemberNodes = members
                           UsedNamespaces = usedNamespaceDeclarations }
                 | TopLevelNode.UsingNamespace ns ->
-                    usings.Add ns
+                    usings.Add(FullNamespaceName.append ns currentNamespaceName)
                 | TopLevelNode.Error msg -> // TODO: Error nodes should be treated as UserState in parser stage, TypeChecker should have to deal with parser errors.
                     addErrorMessage errors node file (UnknownError msg)
 
@@ -84,13 +129,13 @@ module TypeChecker =
                     addErrorMessage errors ty.Identifier.Name ty.Source (DuplicateTypeDefinition ty.Identifier)
 
             for struct(nestedNamespaceName, nested) in nestedNamespaceDeclarations do
-                inner errors file lookup nestedNamespaceName currentUsingDeclarations nested
+                inner errors file lookup (FullNamespaceName.append nestedNamespaceName currentNamespaceName) currentUsingDeclarations nested
 
-        let lookup = Dictionary<TypeIdentifier, CheckingType>()
-        for file in files do inner errors file lookup ImmutableArray.Empty ImmutableArray.Empty file.Nodes
+        let lookup = Dictionary<FullTypeIdentifier, CheckedTypeDefinition>()
+        for file in files do inner errors file lookup FullNamespaceName.empty ImmutableArray.Empty file.Nodes
         lookup :> IReadOnlyDictionary<_, _>
 
-    let private namespaceSearchString (ns: ParsedNamespaceName) =
+    let private namespaceSearchString (FullNamespaceName ns) =
         if not ns.IsDefaultOrEmpty then
             let sb = System.Text.StringBuilder()
             for i = 0 to ns.Length - 1 do
@@ -101,7 +146,7 @@ module TypeChecker =
             String.Empty
 
     let private importedTypeLookup (imports: ImmutableArray<UByte.Resolver.ResolvedModule>) =
-        let lookup = Dictionary<TypeIdentifier, UByte.Resolver.ResolvedTypeDefinition voption>()
+        let lookup = Dictionary<FullTypeIdentifier, UByte.Resolver.ResolvedTypeDefinition voption>()
         fun id ->
             match lookup.TryGetValue id with
             | true, existing -> existing
@@ -115,21 +160,42 @@ module TypeChecker =
                 lookup.Add(id, ty)
                 ty
 
-    type private NamedType = Choice<CheckingType, UByte.Resolver.ResolvedTypeDefinition>
+    type private NamedType = Choice<CheckedTypeDefinition, UByte.Resolver.ResolvedTypeDefinition>
 
     [<IsReadOnly; Struct; NoComparison; NoEquality>]
     type private CheckedTypeAttributes =
         { Flags: Model.TypeDefinitionFlags
           InheritedTypes: ImmutableArray<NamedType> }
 
-    let private checkTypeAttributes errors (declarations: IReadOnlyDictionary<TypeIdentifier, _>) (namedTypeLookup: _ -> _) =
+    let private matchingNamedTypes namedTypeLookup (namespaces: ImmutableArray<FullNamespaceName>) (id: TypeIdentifier) =
+        let matches = List<FullTypeIdentifier * NamedType>()
+
+        let inline search ns =
+            let identifier = FullTypeIdentifier.ofTypeIdentifier ns &id
+            match namedTypeLookup identifier with
+            | ValueSome ty -> matches.Add((identifier, ty))
+            | ValueNone -> ()
+
+        search FullNamespaceName.empty
+        for ns in namespaces do search ns
+
+        matches
+
+    let private findNamedType namedTypeLookup namespaces identifier =
+        let matches = matchingNamedTypes namedTypeLookup namespaces identifier
+        match matches.Count with
+        | 1 -> Ok(snd matches.[0])
+        | 0 -> Error(UndefinedTypeIdentifier identifier)
+        | _ -> Error(AmbiguousTypeIdentifier(identifier, Seq.map fst matches))
+
+    let private checkTypeAttributes errors (declarations: IReadOnlyDictionary<FullTypeIdentifier, _>) namedTypeLookup =
         let lookup = Dictionary<_, CheckedTypeAttributes> declarations.Count
         let inheritedTypeBuilder = ImmutableArray.CreateBuilder<NamedType>()
 
-        for (ty: CheckingType) in declarations.Values do
+        for (ty: CheckedTypeDefinition) in declarations.Values do
             let mutable flags = Model.TypeDefinitionFlags.Final
 
-            for attr in ty.Attributes do
+            for attr in ty.AttributeNodes do
                 match attr.Content with
                 //| TypeAttributeNode.Abstract when flags.IsSet Model.TypeDefinitionFlags.NotFinal
                 //| TypeAttributeNode.Instance when flags.IsSet Model.TypeDefinitionFlags.Abstract ->
@@ -142,16 +208,17 @@ module TypeChecker =
 
             inheritedTypeBuilder.Clear()
 
-            for inheritedTypeName in ty.InheritedTypes do
-                match namedTypeLookup inheritedTypeName with
-                | Ok ty ->
-                    inheritedTypeBuilder.Add ty
-                | Error message ->
-                    addErrorMessage errors inheritedTypeName ty.Source message
+            for inheritedTypeName in ty.InheritanceNodes do
+                match findNamedType namedTypeLookup ty.UsedNamespaces inheritedTypeName.Content with
+                | Ok ty -> inheritedTypeBuilder.Add ty
+                | Error error -> addErrorMessage errors inheritedTypeName ty.Source error
 
             lookup.Add(ty.Identifier, { Flags = flags; InheritedTypes = inheritedTypeBuilder.ToImmutable() })
 
         lookup
+
+    let private checkTypeMembers errors (declarations: IReadOnlyDictionary<FullTypeIdentifier, _>) =
+        ()
 
     let check files imports =
         let errors = ImmutableArray.CreateBuilder()
@@ -159,22 +226,20 @@ module TypeChecker =
 
         let typeImportLookup = importedTypeLookup imports
 
-        let namedTypeLookup ({ ParsedNode.Content = name } as id): Result<NamedType, _> =
-            match declaredTypesLookup.TryGetValue name with
-            | true, declared ->
-                Ok(Choice1Of2 declared)
-            | false, _ ->
-                match typeImportLookup name with
-                | ValueSome imported ->
-                    Ok(Choice2Of2 imported)
-                | ValueNone ->
-                    raise(NotImplementedException("TODO: Handle errors for when type name not found " + name.ToString()))
+        let namedTypeLookup: _ -> NamedType voption =
+            fun id ->
+                match declaredTypesLookup.TryGetValue id with
+                | true, declared -> ValueSome(Choice1Of2 declared)
+                | false, _ ->
+                    match typeImportLookup id with
+                    | ValueSome imported -> ValueSome(Choice2Of2 imported)
+                    | ValueNone -> ValueNone
 
         let typeAttributeLookup = checkTypeAttributes errors declaredTypesLookup namedTypeLookup
 
-        failwith "BAD"
-
-        { CheckedModule.Errors =
+        { CheckedModule.DefinedTypes =
+            failwith "TODO: Get defined types"
+          CheckedModule.Errors =
             if errors.Count <> errors.Capacity
             then errors.ToImmutable()
             else errors.MoveToImmutable() }
--- src/MiddleC.Compiler/Semantics.fsi
+++ src/MiddleC.Compiler/Semantics.fsi
@@ -1,10 +1,41 @@
 ﻿namespace MiddleC.Compiler.Semantics
 
 open System.Collections.Immutable
+open System.Runtime.CompilerServices
+
+open MiddleC.Compiler.Parser
+
+[<IsReadOnly; Struct; NoComparison; StructuralEquality>]
+type FullNamespaceName =
+    internal
+    | FullNamespaceName of ParsedNamespaceName
+
+    interface System.IEquatable<FullNamespaceName>
+
+[<RequireQualifiedAccess>]
+module FullNamespaceName =
+    val empty : FullNamespaceName
+    val append : nested: ParsedNamespaceName -> parent: FullNamespaceName -> FullNamespaceName
+    val toParsedName : FullNamespaceName -> ParsedNamespaceName
+
+[<IsReadOnly; Struct; NoComparison; StructuralEquality>]
+type FullTypeIdentifier =
+    internal
+    | FullTypeIdentifier of TypeIdentifier
+
+    member Name : IdentifierNode
+
+    member Namespace : FullNamespaceName
+
+    override ToString : unit -> string
+
+    interface System.IEquatable<FullTypeIdentifier>
 
 [<NoComparison; NoEquality>]
 type SemanticErrorMessage =
-    | DuplicateTypeDefinition of MiddleC.Compiler.Parser.TypeIdentifier
+    | DuplicateTypeDefinition of FullTypeIdentifier
+    | UndefinedTypeIdentifier of TypeIdentifier
+    | AmbiguousTypeIdentifier of TypeIdentifier * matches: seq<FullTypeIdentifier>
     | UnknownError of message: string
 
     override ToString : unit -> string
@@ -13,15 +44,20 @@ type SemanticErrorMessage =
 type SemanticError =
     { Line: uint32
       Column: uint32
-      Source: MiddleC.Compiler.Parser.ParsedFile
+      Source: ParsedFile
       Message: SemanticErrorMessage }
+
+[<NoComparison; NoEquality>]
+type CheckedTypeDefinition
+
+type CheckedTypeDefinition with
+    member Identifier : FullTypeIdentifier
 
 [<RequireQualifiedAccess; NoComparison; NoEquality>]
 type CheckedModule =
-    { Errors: ImmutableArray<SemanticError> }
+    { DefinedTypes: ImmutableArray<CheckedTypeDefinition>
+      Errors: ImmutableArray<SemanticError> }
 
 [<RequireQualifiedAccess>]
 module TypeChecker =
-    val check :
-        files: ImmutableArray<MiddleC.Compiler.Parser.ParsedFile> ->
-        imports: ImmutableArray<UByte.Resolver.ResolvedModule> -> CheckedModule
+    val check : files: ImmutableArray<ParsedFile> -> imports: ImmutableArray<UByte.Resolver.ResolvedModule> -> CheckedModule
