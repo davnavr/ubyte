73f1d74c6ff6d0d5ad3149d68aa20e6a3343bc9a
David N <davidcalieln@gmail.com>
2021-11-19 19:53:19 -0800

    Add CLI app to handle parsing of .mdlc code

---
+++ src/middlec/Program.fs
@@ -0,0 +1,51 @@
+﻿[<RequireQualifiedAccess>]
+module MiddleC.Compiler.Program
+
+open System.Collections.Immutable
+
+open Argu
+
+[<NoComparison; NoEquality>]
+type Argument =
+    | [<AltCommandLine("-r")>] Import of ``library.binmdl``: string
+    | [<AltCommandLine("-i")>] Input of ``source.mdlc``: string
+    | [<Unique>] Module_Version of uint32 list
+    | [<Unique; AltCommandLine("-o")>] Output of ``output.binmdl``: string
+    //| Parallel
+    | Launch_Debugger
+
+
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Import _ -> "makes the code "
+            | Input _ -> "specifies a source code file to include as part of the compilation"
+            | Module_Version _ -> "specify the version of the compiled module"
+            | Output _ -> "specifies the output file containing the compiled binary module"
+            //| Parallel -> "specify that parsing of source code should be done in parallel"
+            | Launch_Debugger -> "launches the debugger"
+
+let parser = ArgumentParser.Create<Argument>(programName = "middlec")
+
+let parseInputFiles (args: ParseResults<Argument>) =
+    let results = ImmutableArray.CreateBuilder()
+    let rec inner paths =
+        match paths with
+        | [] -> results.ToImmutable()
+        | path :: remaining ->
+            use stream = System.IO.File.OpenRead path
+            results.Add(MiddleC.Compiler.Parser.Parse.fromStream stream)
+            inner remaining
+    inner (args.GetResults <@ Input @>)
+
+[<EntryPoint>]
+let main argv =
+    let args = parser.ParseCommandLine argv
+
+    if args.Contains <@ Launch_Debugger @> then System.Diagnostics.Debugger.Launch() |> ignore
+
+    if args.Contains <@ Import @> then raise(System.NotImplementedException "Module imports are not yet supported")
+
+    let parsedInputFiles = parseInputFiles args
+
+    0 // return an integer exit code
